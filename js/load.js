e737ced4d1a2ee19c64aecf08318af6bc0e05f29
Wei-Cheng Hsieh <esambino.hsieh@gmail.com>
2013-12-17 15:22:45 +0800

    js updated

--- js/load.js
+++ js/load.js
@@ -12,6 +12,12 @@ document.write('<script src="/js/vendor/TweenMax.min.js"></script>');
 document.write('<script src="/js/vendor/jquery.superscrollorama.js"></script>');
 document.write('<script src="/js/vendor/jquery.parallax.min.js"></script>');
 
+//Easing
+document.write('<script src="/js/vendor/jquery.easing.min.js"></script>');
+
+//Lazyload
+document.write('<script src="/js/vendor/jquery.lazyload.min.js"></script>');
+
 //Prefix
 document.write('<script src="/js/vendor/prefixfree.min.js"></script>');
 document.write('<script src="/js/main.js" type="text/javascript"></script>');
