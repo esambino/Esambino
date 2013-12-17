919a5b3a07d42fa1759d09ced885f2cacc74f4f6
Wei-Cheng Hsieh <esambino.hsieh@gmail.com>
2013-12-17 15:18:27 +0800

    js lib

---
+++ js/load.js
@@ -0,0 +1,17 @@
+﻿document.write('<script src="/js/vendor/jquery.min.js"></script>');
+document.write('<script src="/js/vendor/modernizr.js"></script>');
+
+//3D
+document.write('<script src="/js/vendor/three.min.js"></script>');
+document.write('<script src="/js/vendor/PointerLockControls.js"></script>');
+
+//GSAP
+document.write('<script src="/js/vendor/TweenMax.min.js"></script>');
+
+//Scroll 
+document.write('<script src="/js/vendor/jquery.superscrollorama.js"></script>');
+document.write('<script src="/js/vendor/jquery.parallax.min.js"></script>');
+
+//Prefix
+document.write('<script src="/js/vendor/prefixfree.min.js"></script>');
+document.write('<script src="/js/main.js" type="text/javascript"></script>');
---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+    $(window).load(function () {
+        setTimeout(function () {
+            $('.loadingPixel').fadeIn(1500);
+        }, 500);
+        setTimeout(function () {
+            $('#center > h1').fadeIn(1500);
+        }, 1500);
+    });
+
+    $('#center').hover(function () {
+        $('.loadingPixel').children().addClass('slowmo');
+    });
+
+});
